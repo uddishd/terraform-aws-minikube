f7be56a3cb6d3287ecf8a1db1e7f9c435fd71b7e
uddishd <chaitu.chaitanya87@gmail.com>
2024-03-14 17:18:16 +0530

    minikube

---
+++ minikube.tf
@@ -0,0 +1,23 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "roboshop"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/.ssh/dev-aws.pub"
+  aws_subnet_id = "subnet-03d7cb6ee4eb7e310" #replace your subnet id
+  ami_image_id = "ami-0aedf6b1cb669b4c7"
+  hosted_zone = "devaws76s.online"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
